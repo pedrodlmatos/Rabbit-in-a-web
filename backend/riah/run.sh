a625cbcc5f709fee7f2d6c3cb118903d377f5995
Pedro <Matos>
2021-02-08 10:31:42 +0000

    Docker files

--- backend/riah/run.sh
+++ backend/riah/run.sh
@@ -1,3 +1,3 @@
 ls
 cp /app/$CONFIG_FILE /app/application.properties
-java -Xmx32m -Xss256k -XX:+UseSerialGC -XX:MaxRAM=72m -jar es.jar --spring.profiles.default=$PROFILE -Dspring.profiles.active=$PROFILE --spring.profiles.active=$PROFILE
+java -Xmx32m -Xss256k -XX:+UseSerialGC -XX:MaxRAM=72m -jar riah.jar --spring.profiles.default=$PROFILE -Dspring.profiles.active=$PROFILE --spring.profiles.active=$PROFILE
