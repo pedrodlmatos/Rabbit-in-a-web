86261b37aa82b1a3675a56293dbc1c0b09589286
Unknown <pedrolopesmatos@hotmail.com>
2021-03-01 11:45:10 +0000

    vm changes

--- frontend/src/services/environment.js
+++ frontend/src/services/environment.js
@@ -1,9 +1,9 @@
 export const environment = {
-    ETL_URL: 'http://localhost:8100/v1/api/etl/',
+    /*ETL_URL: 'http://localhost:8100/v1/api/etl/',
     TABLE_MAP_URL: 'http://localhost:8100/api/tableMap/',
-    FIELD_MAP_URL: 'http://localhost:8100/api/fieldMap/'
+    FIELD_MAP_URL: 'http://localhost:8100/api/fieldMap/'*/
 
-    //ETL_URL: 'http://35.233.0.56:8100/api/etl/',
-    //TABLE_MAP_URL: 'http://35.233.0.56:8100/api/tableMap/',
-    //FIELD_MAP_URL: 'http://35.233.0.56:8100/api/fieldMap/'
+    ETL_URL: 'http://35.233.0.56:8100/api/etl/',
+    TABLE_MAP_URL: 'http://35.233.0.56:8100/api/tableMap/',
+    FIELD_MAP_URL: 'http://35.233.0.56:8100/api/fieldMap/'
 };
