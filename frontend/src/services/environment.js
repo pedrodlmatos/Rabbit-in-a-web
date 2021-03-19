56b82b1ba37d333625f8b409259454dab7f28879
Unknown <pedrolopesmatos@hotmail.com>
2021-03-19 17:31:47 +0000

    V0.1

--- frontend/src/services/environment.js
+++ frontend/src/services/environment.js
@@ -5,8 +5,8 @@ export const environment = {
     FIELD_MAP_URL: 'http://localhost:8100/v1/api/fieldMap/'
     */
 
-    ETL_URL: 'http://35.225.211.221:8100/v1/api/etl/',
-    TABLE_MAP_URL: 'http://35.225.211.221:8100/v1/api/tableMap/',
-    FIELD_MAP_URL: 'http://35.225.211.221:8100/v1/api/fieldMap/'
+    ETL_URL: 'http://104.199.21.27:8100/v1/api/etl/',
+    TABLE_MAP_URL: 'http://104.199.21.27:8100/v1/api/tableMap/',
+    FIELD_MAP_URL: 'http://104.199.21.27:8100/v1/api/fieldMap/'
 };
 
