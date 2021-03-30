dfb7a21f3293b2e98aa06ad0903a4b3856a03f91
Unknown <pedrolopesmatos@hotmail.com>
2021-03-30 09:39:14 +0100

    Change to a new VM

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 docker stop hiah-backend && docker rm hiah-backend || echo "Backend project not found"
-docker image rm 35.195.9.62:5000/v2/hiah-backend:runtime
-docker run -p 8100:8000 -e CONFIG_FILE="application-runtime.properties" -e spring_profiles_activate='runtime' -e PROFILE="runtime" -d --name=hiah-backend 35.195.9.62:5000/v2/hiah-backend:runtime
+docker image rm 35.195.116.140:5000/v2/hiah-backend:runtime
+docker run -p 8100:8000 -e CONFIG_FILE="application-runtime.properties" -e spring_profiles_activate='runtime' -e PROFILE="runtime" -d --name=hiah-backend 35.195.116.140:5000/v2/hiah-backend:runtime
 docker start hiah-backend
 
 docker stop hiah-frontend && docker rm hiah-frontend || echo "Frontend project not found"
-docker image rm 35.195.9.62:5000/v2/hiah-frontend:runtime
-docker run -p 3000:80 -d --name=hiah-frontend 35.195.9.62:5000/v2/hiah-frontend:runtime
+docker image rm 35.195.116.140:5000/v2/hiah-frontend:runtime
+docker run -p 3000:80 -d --name=hiah-frontend 35.195.116.140:5000/v2/hiah-frontend:runtime
 
 docker start pedrolopesmatos17_postgres_1
