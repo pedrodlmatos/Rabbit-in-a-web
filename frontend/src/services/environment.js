2923fb5de981f5a36d6d2bfc1c74656f68b7d9a1
Unknown <pedrolopesmatos@hotmail.com>
2021-04-06 11:14:16 +0100

    Finalize week02

--- frontend/src/services/environment.js
+++ frontend/src/services/environment.js
@@ -1,5 +1,5 @@
 export const environment = {
-    
+    /*
     ETL_URL: 'http://localhost:8100/v1/api/etl/',
     TARGET_TABLE_URL: 'http://localhost:8100/v1/api/targetTable/',
     TARGET_FIELD_URL: 'http://localhost:8100/v1/api/targetField/',
@@ -7,9 +7,9 @@ export const environment = {
     SOURCE_FIELD_URL: 'http://localhost:8100/v1/api/sourceField/',
     TABLE_MAP_URL: 'http://localhost:8100/v1/api/tableMap/',
     FIELD_MAP_URL: 'http://localhost:8100/v1/api/fieldMap/'
-    
+    */
 
-    /*
+    
     ETL_URL: 'http://104.199.21.27:8100/v1/api/etl/',
     TARGET_TABLE_URL: 'http://104.199.21.27:8100/v1/api/targetTable/',
     TARGET_FIELD_URL: 'http://104.199.21.27:8100/v1/api/targetField/',
@@ -17,6 +17,6 @@ export const environment = {
     SOURCE_FIELD_URL: 'http://104.199.21.27:8100/v1/api/sourceField/',
     TABLE_MAP_URL: 'http://104.199.21.27:8100/v1/api/tableMap/',
     FIELD_MAP_URL: 'http://104.199.21.27:8100/v1/api/fieldMap/'
-    */
+    
 };
 
